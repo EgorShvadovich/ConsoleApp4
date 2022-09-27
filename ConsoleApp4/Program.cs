fd56264431a2242644432e013d224de6eaaa6ef1
Svad_0tmq@student.itstep.org <Svad_0tmq@student.itstep.org>
2022-09-27 16:04:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+static void Main()
+    {
+        int[,] array = new int[10,5];
+        int x = 1;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = x;
+                x++;
+            }
+        }
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
